9dd74baf1758f27e2696af00665231092eb89b07
HectorTarasiuk <165519899+HectorTarasiuk@users.noreply.github.com>
2025-09-29 21:09:05 -0300

    Update PagamentoCartaoCredito.java

--- PagamentoCartaoCredito.java
+++ PagamentoCartaoCredito.java
@@ -1,1 +1,17 @@
+public class PagamentoCartaoCredito implements IPagamento {
+    private String numeroCartao;
 
+    public PagamentoCartaoCredito(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+    @Override
+    public boolean processarPagamento(double valor) {
+        if (valor < 200.0) {
+            System.out.println("Pagamento  aprovado, Valor: " + valor);
+            return true;
+        } else {
+            System.out.println("Pagamento recusado, Valor: " + valor);
+            return false;
+        }
+    }
+}
